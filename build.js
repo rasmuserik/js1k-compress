a8a0ac4c6119c80b8793b3d59bf32da706bbe8ce
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-02-11 14:01:27 +0100

    working, and 1015 bytes

--- build.js
+++ build.js
@@ -1,12 +1,13 @@
 var compress = require('./compress.js');
 var fs = require('fs');
-var string = fs.readFileSync('compress.js', 'utf-8');
-string = compress(string);
+var source= fs.readFileSync('compress.js', 'utf-8');
+string = compress(source);
 var code = 'eval(s=' + string + ')';
 
 s = fs.writeFileSync('entry.js', code);
 s = fs.writeFileSync('entry.js.b64', require('btoa')(require('utf8').encode(code)));
 
-console.log(string.length, eval(string).length, string);
+console.log(string.length, eval(string).length);
+console.log(eval(string)===source);
 console.log();
 console.log(eval(string));
--- compress.js
+++ compress.js
@@ -1,111 +1,57 @@
-/***************************************************
- *                                                 *
- *  Compression module                             *
- *                                                 *
- *                                                 *
- ***************************************************/
+compress = (str) => {
 
-function stringEscape(string) {
-  return string
+  dict = ['\0.']
+
+  str.replace(/[\0-\x20]/g, function(c) {
+    dict[c.charCodeAt(0)] = '\0.'
+  })
+
+  for(i = 0; i < 32; ++i) {
+    if(!dict[i]) {
+      pair = '  '
+      if(str.length > 1) {
+        freq = {}
+
+        for(j = 0; j < str.length - 1; ++j) {
+          pair = str[j] + str[j + 1]
+          freq[pair] = (freq[pair] || 1e8) -1
+        }
+
+        hist = []
+        for(pair in freq) {
+          hist.push(freq[pair] + pair)
+        }
+        hist = hist.sort()
+
+        pair = hist[0].slice(8)
+        str = str.split(pair).join(String.fromCharCode(i))
+      } else {
+        pair = '  '
+      }
+      dict[i] = pair
+    }
+  }
+
+  dict = dict.join('')
+  dict = strEscape(dict)
+  str = strEscape(str)
+
+  return '`' + str + '`.replace(/[\\s\\S]/mg,f=(s,c)=>(d=`' +
+    dict + '`,c=s.charCodeAt(0),g=p=>f(d[p]),c>31||"\0"===d[c*2]?s:g(c*2)+g(c*2+1)))'
+}
+
+strEscape = (str) => {
+  return str
     .replace(/\\/g, '\\\\')
     .replace(/\n/g, '\\n')
     .replace(/\r/g, '\\r')
     .replace(/\0/g, '\\0')
     .replace(/[$]/g, '\\$')
-    .replace(/`/g, '\\`');
-}
-function compress(string) {
-  var dictionarySize = 0;
-  var dictionary = [];
-  dictionary[0] = '\0.';
-  var escapes = {};
-
-  string.replace(/[\0-\x20]/g, function(c) {
-    dictionary[c.charCodeAt(0)] = '\0.';
-  });
-
-  for (i = 0; i < 32; ++i) {
-    if(dictionary[i] === undefined) {
-      if(string.length > 1) {
-        var freq = {};
-        for (j = 0; j < string.length - 1; ++j) {
-          var pair = string[j] + string[j + 1];
-          freq[pair] = (freq[pair] || 100000000) -1;
-        }
-
-        var histogram = [];
-        for (pair in freq) {
-          histogram.push(freq[pair] + pair);
-        }
-        histogram = histogram.sort();
-
-        pair = histogram[0].slice(8);
-        string = string.split(pair).join(String.fromCharCode(i));
-      } else {
-        pair = '  ';
-      }
-      if(pair.length !== 2) {
-        console.log(1742, pair, string[j], string[j+1], histogram[0]);
-      }
-      dictionary[i] = pair;
-    }
-  };
-
-  //console.log(dictionary);
-  dictionary = dictionary.join('');
-  console.log('dictionary size:', dictionary.length);
-  console.log('string size:', string.length);
-  //console.log(dictionary.length, dictionary);
-  //console.log(string.length, string);
-  dictionary = stringEscape(dictionary);
-  string = stringEscape(string);
-
-  return `
-    \`${string}\`
-    .replace(/[\\s\\S]/mg,
-        f=(s,c) => (
-          d = \`${dictionary}\`,
-          c = s.charCodeAt(0),
-          log(c, s, d[c*2], d.length, d),
-          g = p => f(d[p]),
-          c > 31 || '\0' === d[c*2]
-          ? s
-          : g(c*2) + g(c*2+1)
-          ))
-    `;
-  /*
-     return '`' + string + '`' +
-     '.replace(/./mg,' +
-     'f=(s,c)=>(' +
-     'd=`' + dictionary + '`,' +
-     'c=s.charCodeAt(0),' +
-     'g=p=>f(d[p]),' +
-     '(c<31||!d[c*2])?s:g(c*2)+g(c*2+1))'+
-     ')';
-     `;
-     */
-};
-for(var i = 0; i < 32; ++i) {
-  s = '`'+String.fromCharCode(i)+'`';
-  eval(s);
+    .replace(/`/g, '\\`')
 }
 
-function log(o) {
-  //console.log.apply(console, arguments);
-  return o;
-}
-
-if (typeof module === 'object') {
-  module.exports = compress;
-} else if (typeof s === 'string') {
-  document.body.innerHTML = '<pre>' + s + '<pre>';
-}
-s = '\n\n\n';
-s = require('fs').readFileSync('compress.js', 'utf8');
-//s = s.slice(0,10);
-console.log(s.slice(420,430));
-c = compress(s); 
-//console.log(c.length, c);
-//console.log(s);
-console.log(c.length,eval(c).length, eval(c)===s);
-console.log('\n\n\n\n\n');
+(typeof module === 'undefined')
+  ? document.write('<pre>' + 
+      ('> compress(source)\n\n' + compress(s) +
+      '\n\n> eval(compress(source))\n\n' + s).replace('<','&lt;'))
+  : module.exports = compress
